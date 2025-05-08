9145f4632957d47713b1fa983c3c5352063cd0d1
Ilham Dwi Prasetyo <113413951+ilhamidy@users.noreply.github.com>
2025-05-08 12:42:31 +0700

    Update script.js
    update hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const hamburger = document.getElementById('hamburger');
-    const menu = document.getElementById('menu');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
 
-    hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('active');
-      menu.classList.toggle('show');
-    });
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('show');
+  overlay.classList.toggle('show');
+});
+
+overlay.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menu.classList.remove('show');
+  overlay.classList.remove('show');
+});
